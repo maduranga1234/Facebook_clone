396636159c496715814f778e30d9f69944a245df
maduranga <madurangasadaruwan393@gmail.com>
2024-01-30 17:51:50 +0530

    fourth_commit

--- src/Componants/RightBar/RightBar.jsx
+++ src/Componants/RightBar/RightBar.jsx
@@ -4,7 +4,30 @@ import './rightBar.css'
 export default function RightBar() {
   return (
     <div className='rightBarDiv'>
-    <h1>Right bar</h1>
+      <div className='rightBarWrapper'>
+        <div className='birthDayContain'>
+        <img className='birthDayImage' src='\images\gift.png'/> 
+        
+        <span className='birthDayText'>
+       <b> Shehari Parindaya</b> and <b> 3 other friends </b>have a birth day to day
+        </span>
+        </div>
+      <img src='\images\add.jpg' className='addImage'></img> 
+
+      <h4 className='rightBarTital'> Online Friends</h4>
+      <ul className='rightBarFriendLIst'>
+        <li className='rightBarFriend'>
+          <div className='rightBarProfileImageContainer'>
+               <img src='\images\temple.jpg' className='rightBarProfileImage'/> 
+               <span className='online'></span>
+             
+          </div>
+          <span className='rightUserName'>Shehan Rashmika</span>
+        </li>
+      </ul>
+       
+      
+      </div>
     </div>
   )
 }
